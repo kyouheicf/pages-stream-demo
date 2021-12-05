298ed564197acd87c9d064e91d47f138554b1e81
kyouheicf <85217388+kyouheicf@users.noreply.github.com>
2021-12-06 00:30:21 +0900

    Update [id]?.ts

--- functions/admin/videos/[id]?.ts
+++ functions/admin/videos/[id]?.ts
@@ -76,7 +76,7 @@ export async function onRequestPatch(context) {
     }
 
     const res = await fetch(`https://api.cloudflare.com/client/v4/accounts/${env.CF_ACCOUNT_ID}/stream/${id}`, {
-        method: "PUT",
+        method: "POST",
         headers: {
             "Authorization": `Bearer ${env.CF_API_TOKEN_STREAM}`
         },
